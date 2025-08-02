005f8ec1b7eb0051c12e5e2f797541b8349593db
Rolex Oduor <roduor@gmail.com>
2025-08-03 00:53:22 +0300

    Add files via upload

---
+++ fetch-data.js
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+["async", "fetchUserData", "function", "apiUrl", "https://jsonplaceholder.typicode.com/users"]
+=======
+["async", "fetchUserData", "function", "apiUrl", "https://jsonplaceholder.typicode.com/users"]
+
+["dataContainer", "getElementById", "api-data"]
+
+>>>>>>> 382fabd (update fetch-data.js)
+
+["try", "catch", "await", "response.json()"]
+
+ ["dataContainer.innerHTML"]
+
+ ["appendChild", "userList", "document.createElement", "ul"]
+
+  ["Failed to load user data"]
+
+  ["document.addEventListener", "DOMContentLoaded"]
